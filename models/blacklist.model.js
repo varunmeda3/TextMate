39e3e8f4c1c4f282dd3465f63f1311ca3101eb11
varunmeda3 <medavarun3@gmail.com>
2025-01-23 22:37:38 +0530

    change blacklist

--- models/blacklist.model.js
+++ models/blacklist.model.js
@@ -1,13 +1,1 @@
-const mongoose = require("mongoose");
 
-const blacklistSchema = mongoose.Schema({
-     token : String
-})
-
-
-const Blacklist = mongoose.model("blacklist",blacklistSchema)
-
-
-module.exports={
-    Blacklist
-}
